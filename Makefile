77526c297424b362eed18722d7bb16e74c2e1ed0
Youssef-Boulfiham <99402396+Youssef-Boulfiham@users.noreply.github.com>
2026-04-10 22:49:01 +0200

    test run

--- Makefile
+++ Makefile
@@ -7,20 +7,17 @@ endif
 ffmpeg:
 	if test ! -e ffmpeg -a ! -h ffmpeg; then if which ffmpeg > /dev/null; then ln -s $$(which ffmpeg) ./ffmpeg; else curl https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar xJ --absolute-names --no-anchored --transform='s:.*/::' ffmpeg-6.1-amd64-static/ffmpeg; fi; fi
 
-src/frame.o: src/frame.cc src/include/frame.h src/include/consts.h
-	g++ -O2 -fopenmp $(LDFLAGS) -I./src/include -c -o src/frame.o src/frame.cc
+%.o: %.cc %.h consts.h
+	g++ -O2 $(LDFLAGS) -c -o $@ $<
 
-src/animation.o: src/animation.cc src/include/animation.h src/include/consts.h
-	g++ -O2 -fopenmp $(LDFLAGS) -I./src/include -c -o src/animation.o src/animation.cc
-
-julia: main.cc src/frame.o src/animation.o
-	mpic++ -O2 -fopenmp $(LDFLAGS) -I./src/include -o julia main.cc src/frame.o src/animation.o --std=c++23
+julia: main.cc frame.o animation.o consts.h
+	mpic++ -O2 $(LDFLAGS) -o julia main.cc frame.o animation.o --std=c++23
 
 run: ffmpeg julia
-	mpirun --bind-to none -np $(or $(NP),4) ./julia
+	mpirun -np $(or $(NP),4) ./julia
 
 help:
 	@echo "Use \`make\` to build and \`make run\` to execute on 4 processes (default). Use \`make run NP=2\` to choose the number of processes."
 
 clean:
-	rm -f src/frame.o src/animation.o julia
+	rm *.o julia
--- main.cc
+++ main.cc
@@ -1,6 +1,3 @@
-// build: make clean && make
-// run: make run NP=4
-
 #include <cmath>
 #include <complex>
 #include <numbers>
@@ -9,22 +6,20 @@
 #include <iostream>
 #include <mpi.h>
 
-#include "src/include/consts.h"
-#include "src/include/frame.h"
-#include "src/include/animation.h"
+#include "consts.h"
+#include "frame.h"
+#include "animation.h"
 
-#define cimg_display 0        // No window plz
+#define cimg_display 0
 #include "CImg.h"
 
 using std::cout, std::endl;
 using namespace std::literals::complex_literals;
 
-// Colour based on ratio between number of iterations and MAX_ITER
-inline constexpr pixel COLOURISE(double iter) { 
+inline constexpr pixel COLOURISE(double iter) {
   iter = fmod(4 - iter * 5 / MAX_ITER, 6);
   byte x = static_cast<byte>(255 * (1 - std::abs(fmod(iter, 2) - 1)));
   byte r, g, b;
-
   if      (             iter < 1) { r = 255; g =   x; b =   0; }
   else if (iter >= 1 && iter < 2) { r =   x; g = 255; b =   0; }
   else if (iter >= 2 && iter < 3) { r =   0; g = 255; b =   x; }
@@ -34,11 +29,7 @@ inline constexpr pixel COLOURISE(double iter) {
   return { r, g, b };
 }
 
-
-void renderFrame(animation &frames, unsigned int t, unsigned int offset) {
-  frame &f = frames[t - offset];
-
-  // c is constant per frame, varieert per animatie
+void renderFrame(frame &f, unsigned int t) {
   double a = 2 * std::numbers::pi * t / CYCLE_FRAMES;
   double r = 0.7885;
   std::complex<double> c = r * cos(a) + static_cast<std::complex<double>>(1i) * r * sin(a);
@@ -46,58 +37,76 @@ void renderFrame(animation &frames, unsigned int t, unsigned int offset) {
   double x_y_range = 2;
   double scale = 1.5 - 1.45 * log(1 + 9.0 * t / FRAMES) / log(10);
 
+  #pragma omp parallel for schedule(static) num_threads(4)
   for (unsigned int x = 0; x < WIDTH; x++) {
     for (unsigned int y = 0; y < HEIGHT; y++) {
+      double re = (2 * x_y_range * static_cast<double>(x) / WIDTH)  - (x_y_range * 3.0 / 4);
+      double im = (2 * x_y_range * static_cast<double>(y) / HEIGHT) - x_y_range;
+      re *= scale;
+      im *= scale;
 
-      // z berekenen op basis van pixel coordinaat
-      std::complex<double> z = 2 * x_y_range * std::complex(
-          static_cast<double>(x) / WIDTH,
-          static_cast<double>(y) / HEIGHT)
-        - std::complex(x_y_range * 3.0 / 4, x_y_range);
-      z *= scale;
-
-      // Julia iteraties
+      double zr = re, zi = im;
       unsigned int iter = 0;
-      while (std::abs(z) < x_y_range && iter < MAX_ITER) {
-        z = z * z + c;
+      while ((zr*zr + zi*zi) < (x_y_range * x_y_range) && iter < MAX_ITER) {
+        double tmp = zr*zr - zi*zi + c.real();
+        zi = 2*zr*zi + c.imag();
+        zr = tmp;
         iter++;
       }
-
-      // Kleur bepalen
       pixel col = (iter == MAX_ITER) ? pixel{0, 0, 0} : COLOURISE((double)iter);
       f.set_colour(x, y, col);
     }
   }
 }
 
+int main(int argc, char *argv[]) {
+  int provided;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
 
-int main (int argc, char *argv[]) {
-  MPI_Init(&argc, &argv);
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  // Needed to send frames over MPI
-  MPI_Datatype mpi_img;
-  MPI_Type_contiguous(FRAME_SIZE, MPI_BYTE, &mpi_img);
-  MPI_Type_commit(&mpi_img);
+  MPI_Datatype mpi_frame;
+  MPI_Type_contiguous(FRAME_SIZE, MPI_BYTE, &mpi_frame);
+  MPI_Type_commit(&mpi_frame);
 
-  animation frames(FRAMES);
+  // Root initialiseert de volledige animatie
+  animation full;
+  if (rank == 0) full.initialise(FRAMES);
 
-  // TODO - parallellisation
+  // Elke node rendert zijn frames en stuurt ze 1 voor 1 naar root
+  // op de juiste positie via MPI_Send/Recv
   for (unsigned int f = 0; f < FRAMES; f++) {
-    cout << endl << "Rendering frame " << f << endl;
-    renderFrame(frames, f, 0);
+    if ((int)(f % size) == rank) {
+      // Deze node rendert frame f
+      frame local_frame;
+      renderFrame(local_frame, f);
+      if (rank == 0) {
+        // Root heeft het al lokaal, kopieer direct
+        full[f] = local_frame;
+      } else {
+        // Stuur naar root
+        MPI_Send(&local_frame, 1, mpi_frame, 0, f, MPI_COMM_WORLD);
+      }
+    } else if (rank == 0) {
+      // Root ontvangt van de juiste node
+      MPI_Recv(&full[f], 1, mpi_frame, f % size, f, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
   }
 
-  cimg_library::CImg<byte> img(WIDTH,HEIGHT,FRAMES,3);
-  cimg_forXYZ(img, x, y, z) { 
-    img(x,y,z,RED) = (frames)[z].get_channel(x,y,RED);
-    img(x,y,z,GREEN) = (frames)[z].get_channel(x,y,GREEN);
-    img(x,y,z,BLUE) = (frames)[z].get_channel(x,y,BLUE);
+  if (rank == 0) {
+    cout << "All frames gathered, saving video..." << endl;
+    cimg_library::CImg<byte> img(WIDTH, HEIGHT, FRAMES, 3);
+    cimg_forXYZ(img, x, y, z) {
+      img(x,y,z,RED)   = full[z].get_channel(x, y, RED);
+      img(x,y,z,GREEN) = full[z].get_channel(x, y, GREEN);
+      img(x,y,z,BLUE)  = full[z].get_channel(x, y, BLUE);
+    }
+    img.save_video("animation.avi");
+    cout << "Video saved: animation.avi" << endl;
   }
 
-  std::string filename = std::string("animation.avi");
-  img.save_video(filename.c_str());
-
-  // Also needed to send frames over MPI
-  MPI_Type_free(&mpi_img);
+  MPI_Type_free(&mpi_frame);
   MPI_Finalize();
 }
